9c3f121fc4cb3c5d312a480ccd12ae52dd29d6ba
Shreya Samudralwar <61610249+shreya-eng@users.noreply.github.com>
2021-04-11 17:03:27 +0530

    Update unroll.py

--- unroll.py
+++ unroll.py
@@ -11,58 +11,20 @@ import re
 
 
 def unroll(line_, variable, number, flag_=True):
-    """
-    Déroule une ligne de code un certain nombre de fois suivant une unique variable
-    Vous DEVEZ remplacer k par k+0 pour différencier les variables
-    Vous DEVEZ ajuster vous-même les k+=2 dans les boucles
-    Vous EVITEZ d'y passer 10,000 ans
-    :param ligne_: La ligne de code à dérouler
-    :type ligne_: string
-    :param variable: Le nom de la variable à dérouler
-    :type variable: string
-    :param nombre: Nombre de déroulages
-    :type nombre: int
-    :param sens_: True pour croissant (default), False pour décroissant
-    :type sens_: boolean
-    :return: La string correspondant au déroulage demandé
-    :rtype: string => Vous pouvez utiliser derouler DANS derouler DANS derouler !
-    :Example:
-    >>> print(deroulage("lh.at<float>(i-1, k+0);", "i", 3))
-    lh.at<float>(i-1, k+0);
-    lh.at<float>(i+0, k+0);
-    lh.at<float>(i+1, k+0);
-    <BLANKLINE>
-    >>> print(deroulage(deroulage("lh.at<float>(i+0, k-2);", "k", 4), "i", 3, False))
-    lh.at<float>(i+0, k-2);
-    lh.at<float>(i+0, k-1);
-    lh.at<float>(i+0, k+0);
-    lh.at<float>(i+0, k+1);
-    <BLANKLINE>
-    lh.at<float>(i-1, k-2);
-    lh.at<float>(i-1, k-1);
-    lh.at<float>(i-1, k+0);
-    lh.at<float>(i-1, k+1);
-    <BLANKLINE>
-    lh.at<float>(i-2, k-2);
-    lh.at<float>(i-2, k-1);
-    lh.at<float>(i-2, k+0);
-    lh.at<float>(i-2, k+1);
-    <BLANKLINE>
-    <BLANKLINE>
-    """
-    line = line_  # On évite de toucher à la string d'origine
 
-    # On différencie les + et les - pour bien additionner
+    line = line_ 
+
+
     indexs_plus = [m.end() for m in re.finditer(variable + '\+', line)]
     indexs_minus = [m.end() for m in re.finditer(variable + '-', line)]
 
-    # On transforme en liste pour pouvoir modifier le machin
+   
     line = list(line)
     lines = [line[:] for _ in range(number)]
 
     result = []
     for i, line in enumerate(lines):
-        # Sens (+ ou -)
+        
         flag = 1
         if flag_ is False:
             flag = -1
@@ -80,8 +42,8 @@ def unroll(line_, variable, number, flag_=True):
                 line[index - 1] = "+"
             else:
                 line[index - 1] = ""
-        # Retour à une string
+       
         result.append("".join(str(e) for e in line) + "\n")
 
-    # Renvoie une string => Utilisation imbriquée TMTC
+  
     return "".join(str(e) for e in result)
