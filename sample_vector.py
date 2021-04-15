f3e5d6d167e8d3f2bad66128facc1bbb7c026e50
S Shruthi <shruthisk2000@gmail.com>
2021-04-15 18:35:27 +0530

    done vectorization

--- sample_vector.py
+++ sample_vector.py
@@ -1,9 +1,13 @@
 import numpy as np
 
-a = np.random.rand(1000000)
-b = np.random.rand(1000000)
+# dot
+vector_a = np.array([[1, 4], [5, 6]])
+vector_b = np.array([[2, 4], [5, 2]])
+product = np.dot(vector_a, vector_b)
+print("Dot Product : \n", product)
 
-for i in range(0,1000000):
-	c = c + a[i]*b[i]
-
-#dot and cross! 
+# cross
+x = [[1, 2], [3, 4]]
+y = [[5, 6], [7, 8]]
+result = np.cross(x, y)
+print("Cross Product : \n ",result)
