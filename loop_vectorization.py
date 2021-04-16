ee02103181a809f85aeb3254d8b24c0fad72a049
S Shruthi <shruthisk2000@gmail.com>
2021-04-16 17:14:31 +0530

    changes done

--- loop_vectorization.py
+++ loop_vectorization.py
@@ -1,9 +1,10 @@
+import timeit
 import numpy as np
 from math import sin as sn
 import time
 # Number of test points
 N_point  = 1000
-#custom function
+# Define a custom function with some if-else loops
 def myfunc(x,y):
     if (x>0.5*y and y<0.3):
         return (sn(x-y))
@@ -13,11 +14,11 @@ def myfunc(x,y):
         return (2*sn(x+2*y))
     else:
         return (sn(y+x))
-# List of stored elements
+# List of stored elements, generated from a Normal distribution
 lst_x = np.random.randn(N_point)
 lst_y = np.random.randn(N_point)
 lst_result = []
-# First for-loop
+# First,for-loop
 t1=time.time()
 for i in range(len(lst_x)):
     x = lst_x[i]
@@ -31,9 +32,9 @@ for i in range(len(lst_x)):
     else:
         lst_result.append(sn(y+x))
 t2=time.time()
-print("\nTime taken by for loop method\n---------------------------------------\n")
-print(time.time()*1000000) 
-print("micro seconds")
+print("\nTime taken by for-loop\n----------------------------------------------\n{} s".format(1000000*(t2)))
 # Numpy.vectorize method
-vectfunc = np.vectorize(myfunc,otypes=[np.float],cache=False)
-print("\nTime taken by vectorization method\n----------------------------------------------\n{} micro seconds".format(1000000*(t2-t1)))
+print("\nTime taken by numpy.vectorize method\n"+'-'*40)
+vectfunc = np.vectorize(myfunc,otypes=[np.float])
+t3 = time.time()
+print(t3)
